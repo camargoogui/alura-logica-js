e49789590ff472b9c7c51f565b81c5f10d780267
camargoogui <guipcamargo64@gmail.com>
2024-05-16 11:32:04 -0300

    2°cap finalizado. Condicionais e concatenação.

---
+++ exercicios/exCAP2.js
@@ -0,0 +1,33 @@
+// 1.
+// diaSemana = prompt("Qual é o dia da semana?");
+// if (diaSemana == "Sábado") {
+//   alert("Bom fim de semana!");
+// } else if (diaSemana == "Domingo") {
+//   alert("Bom fim de semana!");
+// } else {
+//   alert("Boa semana!");
+// }
+
+// 2.
+// numeroDigitado = prompt("Digite um número positivo ou negativo");
+// if (numeroDigitado > 0) {
+//   alert("Número Positivo!");
+// } else {
+//   alert("Número negativo!");
+// }
+
+// 3.
+// pontuacao = 200;
+// if (pontuacao >= 100) {
+//   alert("Parabéns, você venceu!");
+// } else {
+//   alert("Tente novamente para ganhar.");
+// }
+
+// 4.
+// let saldo = 105.221;
+// alert(`Seu saldo é de R$${saldo}`);
+
+// 5.
+// let nome = prompt("Digite seu nome");
+// alert(`Bem-vindo ${nome}, é um prazer tê-lo aqui`);
