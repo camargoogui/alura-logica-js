5289e609290b7c037f89787b0e0b113e22561b9b
camargoogui <guipcamargo64@gmail.com>
2024-05-16 09:46:20 -0300

    1°cap finalizado + exercicios

---
+++ exercicios/ex00.js
@@ -0,0 +1,32 @@
+// 1.
+// alert("Boas vindas ao nosso site!");
+
+// 2.
+// let nome = "Lua";
+
+// 3.
+// let idade = 25;
+
+// 4.
+// let numeroDeVendas = 50;
+
+// 5.
+// let saldoDisponivel = 1000;
+
+// 6.
+// alert("Erro! Preencha todos os campos");
+
+// 7.
+// let mensagemErro = "Erro! Preencha todos os campos";
+// alert(mensagemErro);
+
+// 8.
+// let nome = prompt("Olá usuário, por favor digite seu nome");
+
+// 9.
+// let idade = prompt("Olá usuário, por favor digite sua idade");
+
+// 10.
+// if (idade >= 18) {
+//   alert("Pode tirar a habilitação!");
+// }
