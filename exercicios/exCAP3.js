8c7c0df0fd1a885f3696a5a2697f916341f8818f
icaroalb1 <icaroalb1senac@gmail.com>
2024-07-10 16:06:49 -0300

    2°cap finalizado. loops e tentativas

---
+++ exercicios/exCAP3.js
@@ -0,0 +1,29 @@
+// 1.
+// let contador = 1;
+// while (contador <= 10){
+//   console.log(contador);
+//   contador++;
+// }
+
+// 2.
+// let contador = 10;
+// while (contador >= 0){
+//   console.log(contador);
+//   contador--;
+// }
+
+// 3.
+// let numero = prompt("Digite um número para a contagem regressiva:");
+// while (numero >= 0){
+//   console.log(numero);
+//   numero--;
+// }
+
+// 4.
+// let numero = prompt("Digite um número para a contagem progressiva:");
+// let contador = 0;
+
+// while (contador <= numero){
+//   console.log(contador);
+//   contador++;
+// }
